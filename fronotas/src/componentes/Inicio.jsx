426e5cb27db19289de35df62596c5e3a651c7958
Jhovan <perjhoal@gmail.com>
2025-01-30 11:08:56 -0600

    Finalizado presentacion, creando form registro

---
+++ fronotas/src/componentes/Inicio.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return ( 
+        <div className="w-5xl mx-auto flex flex-col items-center">
+            <h2 className="py-1.5 font-bold text-gray-700 text-3xl">Notas app</h2>
+            <p className="text-gray-800 text-sl">Hola soy Notas app, alguna vez te has encontrado con la decisión de crear una nota, pero no sabes qué app usar, ya que en una tiene funciones que la otra no tiene, pero terminas pensando que app cuenta con menos publicidad, entonces terminas viendo que en ocasiones te piden generar la compra para evitar publicidad o acceder a más funciones, si es así entonces nos pasa lo mismo entonces te puedo ayudar a crear notas, editarlas, eliminar u organizarlas separándolas por secciones.</p>
+        </div>
+     );
+}
+
+export default Home;
--- fronotas/src/componentes/Principal.jsx
+++ fronotas/src/componentes/Principal.jsx
@@ -1,9 +1,21 @@
+// import Home from "./Inicio";
+
 function Principal() {
     return ( 
-        <>
-            <h2>Notas app</h2>
-            <p>Hola soy Notas app un creador de notas y organizador, cansado de crear solo natas y ver publicadad?, tanquilo </p>
-        </>
+        <div className="w-lg mx-auto">
+            <form className="flex flex-col my-5 items-center justify-center  shadow-2xl rounded ">
+                <label className="text-lg">Usuario</label>
+                <input type="text" placeholder="Crea un usuario" className="p-1.5 font-mono text-center" />
+                <label className="text-lg">Correo</label>
+                <input type="text" placeholder="Ingresa tu correo" className="p-1.5 font-mono text-center" />
+                <label className="text-lg">Contraseña</label>
+                <input type="text" placeholder="Contraseña" className="p-1.5 font-mono text-center" />
+                <label className="text-lg">Repite</label>
+                <input type="text" placeholder="Contraseña" className="p-1.5 font-mono text-center" />
+                <button className="bg-white text-purple-500 mx-1 px-4 py-2 rounded shadow-2xl border-2 cursor-pointer">Registro</button>
+                </form>
+            {/* <Home></Home> */}
+        </div>
      );
 }
 
